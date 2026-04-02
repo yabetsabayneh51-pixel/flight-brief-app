544a96de39dc43a10a7a657c65a8f6132cbf651c
yabetsabayneh51-pixel <yabetsabayneh51@gmail.com>
2026-04-03 01:26:50 +0300

    Update sync.js

--- sync.js
+++ sync.js
@@ -1,1 +1,41 @@
 
+const Sync = (()=>{
+
+const API="PASTE_YOUR_WEBAPP_URL_HERE";
+
+async function api(action,payload={}){
+
+ const res=await fetch(API,{
+  method:"POST",
+  headers:{"Content-Type":"text/plain;charset=utf-8"},
+  body:JSON.stringify({action,payload})
+ });
+
+ return res.json();
+}
+
+async function pullReferenceData(){
+
+ const data=await api("getReference");
+
+ await DB.setAll("airports",data.airports);
+ await DB.setAll("cities",data.cities);
+ await DB.setAll("hotels",data.hotels);
+}
+
+async function push(){
+
+ const briefs=await DB.get("briefs");
+
+ for(const b of briefs.filter(x=>!x.synced)){
+
+  const r=await api("saveBrief",b);
+  if(r.success) b.synced=true;
+ }
+
+ await DB.setAll("briefs",briefs);
+}
+
+return {push,pullReferenceData};
+
+})();
